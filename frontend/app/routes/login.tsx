989d5f592bda6fd90d36824078c876da4631c65a
Aldira Putra Raharja <dira.thea@gmail.com>
2024-10-22 23:39:36 +0200

    feat: nginx example (#8)

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,13 +1,14 @@
 import { startAuthentication } from "@simplewebauthn/browser";
 import { useNavigate } from "@remix-run/react";
+import { useState } from "react";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const urlParams = new URLSearchParams(window.location.search);
+  const redirectUrl = urlParams.get("rd") || "/";
 
   async function login() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const redirectUrl = urlParams.get("rd") || "/";
-
     const register = await fetch("/auth/login", {
       method: "GET",
     });
@@ -17,7 +18,11 @@ export default function Login() {
 
       // POST the response to the endpoint that calls
       // @simplewebauthn/server -> verifyRegistrationResponse()
-      const verificationResp = await fetch("/auth/login", {
+      const queryParams = new URLSearchParams({
+        rd: redirectUrl,
+      });
+      const verificationUrl = `/auth/login?${queryParams.toString()}`;
+      const verificationResp = await fetch(verificationUrl, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -27,9 +32,9 @@ export default function Login() {
 
       if (!verificationResp.ok) {
         throw new Error("Failed to verify login");
-      } else {
-        window.location.replace(redirectUrl);
       }
+
+      setIsLoggedIn(verificationResp.ok);
     } catch (error) {
       console.error(error);
       navigate("/register?rd=" + redirectUrl);
@@ -39,21 +44,30 @@ export default function Login() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-inherit">
       <div className="p-8 rounded shadow-md w-full max-w-md flex justify-center">
-      <form
-        onSubmit={async (e) => {
-        e.preventDefault();
-        await login();
-        }}
-      >
-        <div className="flex justify-center">
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Login with passkey
-        </button>
-        </div>
-      </form>
+        {isLoggedIn ? (
+          <a
+            href={redirectUrl}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Continue
+          </a>
+        ) : (
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              await login();
+            }}
+          >
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Login with passkey
+              </button>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
